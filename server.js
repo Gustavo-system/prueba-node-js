d1109518c55cda26e01a166f28dc80ef85d9b133
Gustavo <f170015@gmail.com>
2021-07-23 00:08:56 -0500

    checkin server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const db = require('./db/db_conect');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
 db(`mongodb+srv://db_gus_user:1234567890@cluster0.cr7zd.mongodb.net/db_prueba`)
 
 app.use(express.json());
@@ -13,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 try {
   router(app);
   app.listen(process.env.PORT, () => {
-    console.log(`servidor escuchando en http://localhost:${port}`);
+    console.log(`servidor escuchando en http://localhost:${process.env.PORT}`);
   });
 } catch (error) {
   console.log(`no se puede inciar el servidor ${error}`);
